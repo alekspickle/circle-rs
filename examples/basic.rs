dc19c0eead46f77b0b8f5496958f1811133bdf21
Aleks Gnatovskyi <aleks.work2222@gmail.com>
2020-01-07 16:51:18 +0200

    examples: update basic

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,15 +2,16 @@ use std::{io::Result, thread, time::{Duration, Instant}};
 use circle_rs::{Infinite, Progress};
 
 pub fn main() -> Result<()> {
-    println!("\nGoing to poll some stuff.");
+    println!("\n100 ms delay");
     let mut loader = Infinite::new().to_stderr();
     loader.set_msg("Polling");
-    loader.start()?;
+    
+    let start_thread = loader.start()?;
     let now = Instant::now();
     thread::sleep(Duration::from_secs(2));
-    println!("elapsed {:?}", now.elapsed());
     loader.stop()?;
-
+    println!("elapsed {} {:?}",start_thread, now.elapsed());
+    
     Ok(())
 }
 
